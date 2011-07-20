578aa5c2e7b8f09456818e8a0aa0a458824ff37c
Timur Valeev <timur.valeev@flatsoft.com>
2011-07-20 10:03:32 +0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkLog::Application.config.secret_token = '76533d7e4cec026683ba4f81c60f02f8435152f68b9f29db36ed7e806d00091caa8fcb32c9897857c6be899f8fca35b61de82411cefb91f6c45c1cbfc5d9e1c3'
