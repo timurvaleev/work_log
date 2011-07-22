3f64af99ed27f0f387aa8ee1cc1c3d79c98f0b1a
Timur Valeev <timur.valeev@flatsoft.com>
2011-07-22 12:50:55 +0400

    Bug with date_for field

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,7 @@
 class Plan
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::MultiParameterAttributes
   field :date_for, :type => Date
   field :actual_log, :type => String
   field :report_log, :type => String
